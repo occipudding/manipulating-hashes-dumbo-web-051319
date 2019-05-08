e488f981343443d020f111a867619ff962d0e210
Joshua Suskin <jsuskin85@gmail.com>
2019-05-08 20:54:14 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,19 @@
+require 'pry'
+
 def second_challenge
+  vals = []
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
    vegetable: ["carrots", "broccoli", "cucumbers"],
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
-  #code your solution here!
-
-  
-
+  groceries.each do |k, v|
+    v.each do |item|
+        binding.pry
+      end
+    end
+  vals
 end
+
+second_challenge
